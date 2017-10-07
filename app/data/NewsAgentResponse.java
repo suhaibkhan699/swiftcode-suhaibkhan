8bcef20f5644e12712cd4970af2e6d69c7f23229
suhaibkhan <khansuhaib699@gmail.com>
2017-10-07 10:02:10 +0000

    message1

--- app/data/NewsAgentResponse.java
+++ app/data/NewsAgentResponse.java
@@ -1,5 +1,7 @@
 package data;
 
+import play.libs.ws.WSRequest;
+
 public class NewsAgentResponse {
     public String keyword;
     public String intent;
--- app/services/FeedService.java
+++ app/services/FeedService.java
@@ -12,6 +12,25 @@ import java.util.concurrent.ExecutionException;
 
 public class FeedService {
 public FeedResponse getFeedResponse(String keyword){
-    return null;
-}
+    FeedResponse feedResponseObject=new FeedResponse();
+    try{
+        WSRequest feedRequest=WS.url("https://news.google.com/news");
+        CompletionStage<WSResponse> responsePromise=feedRequest
+                .setQueryParameter("q",keyword)
+                .setQueryParameter("output","rss")
+                .get();
+        Document feedResponse = responsePromise.thenApply(WSResponse::asXml).toCompletableFuture().get();
+    Node item=feedResponse.getFirstChild().getFirstChild().getChildNodes().item(9);
+    feedResponseObject.title=item.getChildNodes().item(0).getFirstChild().getNodeValue();
+    feedResponseObject.pubDate=item.getChildNodes().item(3).getFirstChild().getNodeValue();
+    feedResponseObject.description=item.getChildNodes().item(4).getFirstChild().getNodeValue();
+
+    }
+    catch(Exception e)
+    {
+        e.printStackTrace();
+
+    }
+return feedResponseObject;
+
 }

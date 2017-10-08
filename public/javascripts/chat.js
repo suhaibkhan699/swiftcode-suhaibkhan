3ff0fdf6e3c61e39a0e2a154e5649733c4802ab5
suhaibkhan <khansuhaib699@gmail.com>
2017-10-08 09:45:04 +0000

    finally working

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -7,7 +7,7 @@ app.controller('chatController', function ($scope, $sce) {
 
 
 
-var  exampleSocket =  new  WebSocket("wss://swiftcode-ws-chat.herokuapp.com/chatSocket");
+var  exampleSocket =  new  WebSocket('ws://localhost:9000/chatSocket');
     exampleSocket.onmessage  =   function  (event) {
         var jsonData = JSON.parse(event.data);
         jsonData.time = new Date()

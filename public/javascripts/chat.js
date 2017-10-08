666bd567bb7f5ab87ea30df691fd6ef9903b9177
suhaibkhan <khansuhaib699@gmail.com>
2017-10-08 05:52:16 +0000

    day3_break

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,4 +1,27 @@
 var app = angular.module('chatApp', ['ngMaterial']);
 
 app.controller('chatController', function ($scope, $sce) {
+
+    $scope.messages=[
+    {
+    'sender':'USER',
+    'text':'hi',
+    'time':'10:01 AM'
+    },
+    {
+    'sender':'BOT',
+    'text':'hello',
+    'time':'10:01 AM'
+    },
+    {
+    'sender':'USER',
+    'text':'I love swiftcode',
+    'time':'10:01 AM'
+    },
+    {
+    'sender':'BOT',
+    'text':"news",
+    'time':'10:01 AM'
+    }
+]
 });
